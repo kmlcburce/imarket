05513f528aebd87b1b76e87bd7057bcdf4a8adfb
kennCK <kennettecanales@gmail.com>
2020-07-03 03:22:59 +0800

    update

--- merchant/src/routes/web.php
+++ merchant/src/routes/web.php
@@ -5,6 +5,7 @@ $route = env('PACKAGE_ROUTE', '').'/merchants/';
 $controller = 'Increment\Imarket\Merchant\Http\MerchantController@';
 Route::post($route.'create', $controller."create");
 Route::post($route.'retrieve', $controller."retrieve");
+Route::post($route.'retrieve_all', $controller."retrieveAll");
 Route::post($route.'update', $controller."update");
 Route::post($route.'delete', $controller."delete");
 Route::get($route.'test', $controller."test");
