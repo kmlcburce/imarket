d6e441d7d0916adb2cc59ced234d9658af0a245b
keyrwinz <keyrwinfelisilda@gmail.com>
2020-08-01 16:08:05 +0800

    update CartController

--- cart/src/https/CartController.php
+++ cart/src/https/CartController.php
@@ -12,4 +12,23 @@ class CartController extends APIController
     $this->model = new Cart();
   }
 
+  public function create(Request $request){
+    $data = $request->all();
+    $data['code'] = $this->generateCode();
+    $this->model = new Cart();
+    $this->insertDB($data);
+    return $this->response();
+  }
+
+
+  public function generateCode(){
+    $code = 'CRT-'.substr(str_shuffle($this->codeSource), 0, 60);
+    $codeExist = Cart::where('code', '=', $code)->get();
+    if(sizeof($codeExist) > 0){
+      $this->generateCode();
+    }else{
+      return $code;
+    }
+  }
+
 }
