2ffc26e92ae9fb326ce7b13f3d314fb93a231d75
kennCK <kennettecanales@gmail.com>
2020-08-17 20:04:42 +0800

    update

--- cart/src/routes/web.php
+++ cart/src/routes/web.php
@@ -5,6 +5,7 @@ $route = env('PACKAGE_ROUTE', '').'/checkouts/';
 $controller = 'Increment\Imarket\Cart\Http\CheckoutController@';
 Route::post($route.'create', $controller."create");
 Route::post($route.'retrieve', $controller."retrieve");
+Route::post($route.'retrieve_orders', $controller."retrieveOrders");
 Route::post($route.'update', $controller."update");
 Route::post($route.'delete', $controller."delete");
 Route::get($route.'test', $controller."test");
