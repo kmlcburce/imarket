3d5a9b7d6a67603162bc68a2bf2480f689e60207
Ean Jason <ecvelayo@gmail.com>
2020-08-17 11:30:15 +0800

    Update db

--- location/src/routes/web.php
+++ location/src/routes/web.php
@@ -4,3 +4,6 @@ $route = env('PACKAGE_ROUTE', '').'/locations/';
 $controller = 'Increment\Imarket\Location\Http\LocationController@';
 Route::post($route.'create', $controller."create");
 Route::post($route.'retrieve', $controller."retrieve");
+Route::post($route.'update', $controller."update");
+Route::post($route.'delete', $controller."delete");
+Route::get($route.'test', $controller."test");
