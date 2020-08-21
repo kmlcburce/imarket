9a2ea491e811d686603a2a63caf258eeff32c9ea
kennCK <kennettecanales@gmail.com>
2020-08-21 16:46:11 +0800

    update

--- delivery/src/routes/web.php
+++ delivery/src/routes/web.php
@@ -5,6 +5,7 @@ $route = env('PACKAGE_ROUTE', '').'/deliveries/';
 $controller = 'Increment\Imarket\Delivery\Http\DeliveryController@';
 Route::post($route.'create', $controller."create");
 Route::post($route.'retrieve', $controller."retrieve");
+Route::post($route.'my_deliveries', $controller."myDeliveries");
 Route::post($route.'update', $controller."update");
 Route::post($route.'delete', $controller."delete");
 Route::get($route.'test', $controller."test");
