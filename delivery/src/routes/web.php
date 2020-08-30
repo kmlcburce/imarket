b98f0e47f27a1d96dfc1a26b1b89618bb5004911
ecvelayo <ecvelayo@gmail.com>
2020-08-30 20:51:09 +0800

    Changed schedule type in db

--- delivery/src/routes/web.php
+++ delivery/src/routes/web.php
@@ -10,7 +10,7 @@ Route::post($route.'delete', $controller."delete");
 Route::get($route.'test', $controller."test");
 
 //delivery fees
-$route = env('PACKAGE_ROUTE', '').'/delivy_fees/';
+$route = env('PACKAGE_ROUTE', '').'/delivery_fees/';
 $controller = 'Increment\Imarket\Delivery\Http\DeliveryFeeController@';
 Route::post($route.'create', $controller."create");
 Route::post($route.'retrieve', $controller."retrieve");
