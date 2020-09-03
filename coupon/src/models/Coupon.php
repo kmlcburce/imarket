859d78d68c8b9f9dc58de3424ffbe3ffacd21de8
Kennette Canales <kennettecanales@gmail.com>
2020-09-03 16:22:59 +0800

    update

--- coupon/src/models/Coupon.php
+++ coupon/src/models/Coupon.php
@@ -11,12 +11,12 @@ class Coupon extends APIModel
 
     protected $dates = ['start', 'end'];
     protected $dateFormat = 'Y-m-d H:i:s';
-    public function getStartAttribute($date)
-    {
-      return Carbon::createFromFormat('Y-m-d H:i:s', $date)->copy()->tz('Asia/Manila')->format('F j, Y g:i A');
-    }
-    public function getEndAttribute($date)
-    {
-      return Carbon::createFromFormat('Y-m-d H:i:s', $date)->copy()->tz('Asia/Manila')->format('F j, Y g:i A');
-    }
+    // public function getStartAttribute($date)
+    // {
+    //   return Carbon::createFromFormat('Y-m-d H:i:s', $date)->copy()->tz('Asia/Manila')->format('F j, Y g:i A');
+    // }
+    // public function getEndAttribute($date)
+    // {
+    //   return Carbon::createFromFormat('Y-m-d H:i:s', $date)->copy()->tz('Asia/Manila')->format('F j, Y g:i A');
+    // }
 }
