792cdf516b18d882f714af0103d73a71bfd00495
kennCK <kennettecanales@gmail.com>
2020-09-07 23:27:48 +0800

    update

--- merchant/src/routes/web.php
+++ merchant/src/routes/web.php
@@ -7,5 +7,6 @@ Route::post($route.'create', $controller."create");
 Route::post($route.'retrieve', $controller."retrieve");
 Route::post($route.'retrieve_all', $controller."retrieveAll");
 Route::post($route.'update', $controller."update");
+Route::post($route.'update_by_verification', $controller."updateByVerification");
 Route::post($route.'delete', $controller."delete");
 Route::get($route.'test', $controller."test");
