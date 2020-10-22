ce8f714cafb3f3e42189f7120aafddae58f366ad
ecvelayo <ecvelayo@gmail.com>
2020-10-22 09:17:49 +0800

    added status on variations

---
+++ product/src/migrations/2020_10_22_011526_update_product_attributes_table_add_status_variations.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateProductAttributesTableAddStatusVariations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('product_attributes', function (Blueprint $table) {
+            $table->string('status')->after('currency');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
