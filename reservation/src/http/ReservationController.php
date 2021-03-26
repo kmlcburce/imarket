51cde3c9a9e8459b754c25a78e49dad260c2c926
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-03-26 14:50:18 +0800

    addtional fields in retrieve reservations

--- reservation/src/http/ReservationController.php
+++ reservation/src/http/ReservationController.php
@@ -13,4 +13,20 @@ class ReservationController extends APIController
    	function __construct(){
    		$this->model = new Reservation();
    	}
+
+	public function retrieve(Request $request){
+		$data = $request->all();
+		$con = $data['condition'];
+		$result = Reservation::where($con[0]['column'], $con[0]['clause'], $con[0]['value'])->offset($data['offset'])->limit($data['limit'])->get();
+		if(sizeof($result) > 0){
+			$i = 0;
+			foreach ($result as $key) {
+				$result[$i]['reservee'] = $this->retrieveNameOnly($result[$i]['account_id']);
+
+			 $i++;
+			}
+			$this->response['data'] = $result;
+		}
+		return $this->response();
+	}
 }
