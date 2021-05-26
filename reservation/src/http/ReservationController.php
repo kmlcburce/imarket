b3535400dbb56e922d35206db62c267d3f2ea1a5
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-05-26 16:31:11 +0800

    additional data (eg. superLike & distance)

--- reservation/src/http/ReservationController.php
+++ reservation/src/http/ReservationController.php
@@ -15,6 +15,8 @@ class ReservationController extends APIController
     public $merchantClass = 'Increment\Imarket\Merchant\Http\MerchantController';
 	public $messengerGroupClass = 'Increment\Messenger\Http\MessengerGroupController';
 	public $ratingClass = 'Increment\Common\Rating\Http\RatingController';
+	public $topChoiceClass = 'App\Http\Controllers\TopChoiceController';
+	public $locationClass = 'Increment\Common\Location\Http\LocationController';
 
    	function __construct(){
    		$this->model = new Reservation();
@@ -31,6 +33,8 @@ class ReservationController extends APIController
 				$result[$i]['reservee'] = $this->retrieveNameOnly($result[$i]['account_id']);
 				$result[$i]['synqt'] = app($this->synqtClass)->retrieveByParams('id', $result[$i]['payload_value']);
                 $result[$i]['merchant'] = app($this->merchantClass)->getByParams('id', $result[$i]['merchant_id']);
+				$result[$i]['distance'] = app($this->locationClass)->getLocationDistance('merchant_id', $result[$i]['merchant_id'], $data['merchant_id']);
+				$result[$i]['total_super_likes'] = app($this->topChoiceClass)->countByParams('merchant_id', $result[$i]['merchant_id'], 'super-like');
 				$result[$i]['rating'] = app($this->ratingClass)->getRatingByPayload('merchant_id', $result[$i]['merchant_id']);
 				$result[$i]['date_time_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result[$i]['datetime'])->copy()->tz($this->response['timezone'])->format('F j, Y H:i A');
 				$result[$i]['members'] = app($this->messengerGroupClass)->getMembersByParams('payload', $result[$i]['payload_value'], ['id', 'title']);
