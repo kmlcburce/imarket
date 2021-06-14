1a82686f238a03b397eeefa8d308e00c56820413
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-06-14 17:30:07 +0800

    retrieve merchant

--- merchant/src/http/MerchantController.php
+++ merchant/src/http/MerchantController.php
@@ -118,7 +118,6 @@ class MerchantController extends APIController
               $merchant[$m]['account'] = $this->retrieveAccountDetails($merchantKey['account_id']);
               $merchant[$m]['rating'] = app('Increment\Common\Rating\Http\RatingController')->getRatingByPayload('account_id', $merchantKey['account_id']);
 
-              dd($merchant);
             }
             $i++;
           }
